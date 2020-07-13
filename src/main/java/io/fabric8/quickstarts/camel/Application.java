505c001dfdf655b2ff157387ca564dd9a45a3313
Kyle Le <kle@kle.remote.csb>
2020-07-13 15:12:53 -0700

    Testing

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -37,8 +37,9 @@ public class Application extends RouteBuilder {
     	from("timer://foo?period=5000")
           .setBody().constant("Hello World")
         //  .to("netty:tcp://172.30.34.159:3100?sync=false");
-	      .log("Sending to route inbound-route-openshift.apps-crc.testing on port 80 using netty http")
-    	  .to("netty-http:http://inbound-route-openshift.apps-crc.testing:80?sync=false");
+	      .log("Sending to route inbound-route-openshift.apps-crc.testing on port 80 using netty tcp")
+	      .to("netty:tcp://inbound-route-openshift.apps-crc.testing:80?sync=false");
+    	//  .to("netty-http:http://inbound-route-openshift.apps-crc.testing:80?sync=false");
         //    .log(">>> ${body}");
     }
 }
