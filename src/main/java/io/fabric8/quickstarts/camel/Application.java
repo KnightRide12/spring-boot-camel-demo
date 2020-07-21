158d71ddb5a75ab35992b410a5546c62c8d09ab2
Kyle Le <kle@kle.remote.csb>
2020-07-21 14:59:00 -0700

    Testing amqp

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -15,8 +15,13 @@
  */
 package io.fabric8.quickstarts.camel;
 
+import org.apache.activemq.jms.pool.PooledConnectionFactory;
+import org.apache.camel.component.amqp.AMQPComponent;
+import org.apache.camel.component.jms.JmsConfiguration;
+import org.apache.qpid.jms.JmsConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
 /**
@@ -29,5 +34,14 @@ public class Application {
     // must have a main method spring-boot can run
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean(name="amqp-component")
+    AMQPComponent amqpComponent(AMQPConfiguration config) {
+    	JmsConnectionFactory qpid = new JmsConnectionFactory(config.getUsername(), config.getPassword(), config.getUrl());
+    	PooledConnectionFactory factory = new PooledConnectionFactory();
+    	factory.setConnectionFactory(qpid);
+    	
+    	return new AMQPComponent(factory);
     }
 }
