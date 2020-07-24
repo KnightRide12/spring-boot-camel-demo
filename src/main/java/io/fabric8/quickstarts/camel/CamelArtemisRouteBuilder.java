64265b0bf9a355287342b0871b0edb98d127b733
Kyle Le <kle@kle.remote.csb>
2020-07-23 19:52:54 -0700

    Testing

--- src/main/java/io/fabric8/quickstarts/camel/CamelArtemisRouteBuilder.java
+++ src/main/java/io/fabric8/quickstarts/camel/CamelArtemisRouteBuilder.java
@@ -3,19 +3,16 @@ package io.fabric8.quickstarts.camel;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CamelArtemisRouteBuilder extends RouteBuilder {
 
-	@Autowired
-	JmsTemplate jmsTemplate;
+	//@Autowired
+	//JmsTemplate jmsTemplate;
 	
-	@Value("${jms.queue.destination}")
-	String destinationQueue;
+	//@Value("${jms.queue.destination}")
+	//String destinationQueue;
 	
 	@Override
 	public void configure() throws Exception {
@@ -27,26 +24,26 @@ public class CamelArtemisRouteBuilder extends RouteBuilder {
 				log.info("Received: " + arg0.getIn().getBody(String.class));
 			}
     	  })
-          .setBody().constant("Hello1")
-    	  .to("jms:queue:demoQueue")
-          .log("Delivered to jms:queue:demoQueue")
-          .setBody().constant("Hello2")
-          .to("jms:topic:demoTopic")
-          .log("Delivered to jms:topic:demoTopic")
-          .setBody().constant("Hello3")
+          //.setBody().constant("Hello1")
+    	  //.to("jms:queue:demoQueue")
+          //.log("Delivered to jms:queue:demoQueue")
+          //.setBody().constant("Hello2")
+          //.to("jms:topic:demoTopic")
+          //.log("Delivered to jms:topic:demoTopic")
+          //.setBody().constant("Hello3")
           .to("jms:queue:demoTopic")
-          .log("Delivered to jms:queue:demoTopic")
-          .setBody().constant("Hello4")
-          .to("jms:topic:demoQueue")
-          .log("Delivered to jms:topic:demoQueue")
-          .setBody().constant("Hello5")
-          .to("jms:topic:demoTopic.demoQueue")
-          .log("Delivered to jms:topic:demoTopic.demoQueue")
-          .setBody().constant("Hello6")
-          .to("jms:queue:demoTopic.demoQueue")
-          .log("Delivered to jms:queue:demoTopic.demoQueue");
+          .log("Delivered to jms:queue:demoTopic");
+          //.setBody().constant("Hello4")
+          //.to("jms:topic:demoQueue")
+          //.log("Delivered to jms:topic:demoQueue")
+          //.setBody().constant("Hello5")
+          //.to("jms:topic:demoTopic.demoQueue")
+          //.log("Delivered to jms:topic:demoTopic.demoQueue")
+          //.setBody().constant("Hello6")
+          //.to("jms:queue:demoTopic.demoQueue")
+          //.log("Delivered to jms:queue:demoTopic.demoQueue");
 		
-		  jmsTemplate.convertAndSend(destinationQueue, "Hello!!");
+		  //jmsTemplate.convertAndSend(destinationQueue, "Hello!!");
           
 
 	}
