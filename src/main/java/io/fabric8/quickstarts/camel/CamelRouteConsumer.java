11e41883300368f43e3f87ad35b3f52956c45906
Kyle Le <kle@kle.remote.csb>
2020-11-04 10:36:10 -0800

    Adding consumer

---
+++ src/main/java/io/fabric8/quickstarts/camel/CamelRouteConsumer.java
@@ -0,0 +1,32 @@
+package io.fabric8.quickstarts.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.hl7.HL7MLLPNettyDecoderFactory;
+import org.apache.camel.component.hl7.HL7MLLPNettyEncoderFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CamelRouteConsumer extends RouteBuilder {
+
+	@Bean
+	private HL7MLLPNettyEncoderFactory hl7Encoder() {
+	  HL7MLLPNettyEncoderFactory encoder = new HL7MLLPNettyEncoderFactory();
+	  encoder.setCharset("iso-8859-1");
+	  return encoder;
+	}
+	
+	@Bean
+	private HL7MLLPNettyDecoderFactory hl7Decoder() {
+	  HL7MLLPNettyDecoderFactory decoder = new HL7MLLPNettyDecoderFactory();
+	  decoder.setCharset("iso-8859-1");
+	  return decoder;
+	}
+	  
+	
+	@Override
+	public void configure() throws Exception {
+		from("jms:queue:demoTopic")
+    	  .log("Message received from JMS Queue: ${body}");
+	}
+}
